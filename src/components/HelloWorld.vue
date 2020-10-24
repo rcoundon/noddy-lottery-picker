61f1f4eb5d421cd5e8f16a279bef694cab37ff1f
Ross Coundon <ross.coundon@leadent.digital>
2020-10-24 19:18:28 +0100

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <b-field>
+      <b-input :value="numbers[0]"></b-input>
+      <b-input :value="numbers[1]"></b-input>
+      <b-input :value="numbers[2]"></b-input>
+      <b-input :value="numbers[3]"></b-input>
+      <b-input :value="numbers[4]"></b-input>
+      <b-input :value="numbers[5]"></b-input>
+    </b-field>
+    <b-button @click="generateRandomNumbers">Generate!</b-button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+import Vue from 'vue';
+function generateRandomNumber(existingNumbers) {
+  let min = 1;
+  let max = 59;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let result = Math.floor(Math.random() * (max - min) + min);
+  if (existingNumbers.includes(result)) {
+    return generateRandomNumber(existingNumbers);
+  } else {
+    return result;
   }
 }
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      numbers: [0, 0, 0, 0, 0, 0],
+    };
+  },
+  methods: {
+    generateRandomNumbers() {
+      let number0 = generateRandomNumber(this.numbers);
+      let number1 = generateRandomNumber(this.numbers);
+      let number2 = generateRandomNumber(this.numbers);
+      let number3 = generateRandomNumber(this.numbers);
+      let number4 = generateRandomNumber(this.numbers);
+      let number5 = generateRandomNumber(this.numbers);
+      Vue.set(this.numbers, 0, number0);
+      Vue.set(this.numbers, 1, number1);
+      Vue.set(this.numbers, 2, number2);
+      Vue.set(this.numbers, 3, number3);
+      Vue.set(this.numbers, 4, number4);
+      Vue.set(this.numbers, 5, number5);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
